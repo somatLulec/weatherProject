76afd09f902e112fcadd5e14bee8231ac3c205e7
Tomáš Mátl <somat@volny.cz>
2023-12-01 15:35:18 +0100

    Initial revision

---
+++ wheather.py
@@ -0,0 +1,74 @@
+
+import tkinter as tk
+import requests
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import ttkbootstrap
+
+
+def get_weather(city):
+    API_key = "7ef2edbd7b0a1242681a19d0327096c6"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
+
+    #print(url)
+
+    res = requests.get(url)
+
+    if res.status_code == 404:
+        messagebox.showerror("Error", "Město nenalezeno.")
+        return None
+
+    weather = res.json()
+    icon_id = weather['weather'][0]['icon']
+    temperature = weather['main']['temp'] - 273.15
+    description = weather['weather'][0]['description']
+    city = weather['name']
+    country = weather['sys']['country']
+
+    icon_url = f"https://openweathermap.org/img/wn/{icon_id}@2x.png"
+    return (icon_url, temperature, description, city, country)
+
+def search():
+    city = city_entry.get()
+    result = get_weather(city)
+    if result is None:
+        return
+
+    icon_url, temperature, description, city, country = result
+    location_label.configure(text=f"{city}, {country}")
+
+    image = Image.open(requests.get(icon_url, stream=True).raw)
+    icon = ImageTk.PhotoImage(image)
+    icon_label.configure(image=icon)
+    icon_label.image = icon
+
+    temperature_label.configure(text=f"Teplota {temperature:2f}°C")
+    description_label.configure(text=f"Popis {description}")
+
+
+root = ttkbootstrap.Window(themename="morph")
+root.title = "Předpověď počasí"
+root.geometry("400x400")
+
+city_entry = ttkbootstrap.Entry(root, font="Helvetica, 18")
+city_entry.pack(pady=10)
+
+search_button = ttkbootstrap.Button(root, text="Hledat", command=search, bootstyle="warning")
+search_button.pack(pady=10)
+
+location_label = tk.Label(root, font="Helvetoce, 25")
+location_label.pack(pady=20)
+
+icon_label = tk.Label(root)
+icon_label.pack()
+
+temperature_label = tk.Label(root, font="Helvetica, 20")
+temperature_label.pack()
+
+description_label = tk.Label(root, font="Helvetica, 20")
+description_label.pack()
+
+root.mainloop()
+
+
+
