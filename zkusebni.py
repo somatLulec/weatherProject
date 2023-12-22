c3806464ab80aa1d8abdabf588992d16b399ec0b
Tomáš Mátl <somat@volny.cz>
2023-12-22 14:29:08 +0100

    pokracovani

--- zkusebni.py
+++ zkusebni.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 window = tk.Tk()
-window.geometry("400x300")
+window.geometry("500x400")
 window.title("Předpověď počasí")
 
 PX=5
@@ -13,8 +13,10 @@ PY=10
 API_key = "7ef2edbd7b0a1242681a19d0327096c6"
 c1Var = []
 sel = tk.StringVar()
+#data o meste a poloze
+data = {}
 
-def get_cesky(popis):
+def get_cesky_pocasi(popis):
     match popis:
         case "overcast clouds":
             return "zataženo, mraky"
@@ -30,17 +32,25 @@ def get_cesky(popis):
             return "slabé sněžení"
     return popis
 
+def get_cesky_kraje(region):
+
+    return region
+
+
 def set_mesto(*args):
     e1.delete(0, "end")
     e1.insert(0, c1.get())
 
 def set_box(jmesta):
+    data.clear()
+    i = -1
     for mesto in jmesta:
-        for mesta in jmesta:
-            if mesta["name"]:
-                if c1Var.count(mesta["name"]) == 0:
-                    c1Var.append(mesta["name"])
-
+        i += 1
+        combo = mesto['name'] + ', ' + get_cesky_kraje(mesto['state']) + ', ' + mesto['country']
+        a = {'mesto': mesto['name'], 'lat': mesto['lat'], 'lon': mesto['lon'], 'state': mesto['state'],
+             'country': mesto['country'], 'combo': combo}
+        data[i] = a
+        c1Var.append(combo)
 
 def get_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
@@ -63,9 +73,21 @@ def get_weather(city):
 
         jmesta = res1.json()
 
+
         #naplneni mest
         set_box(jmesta)
 
+        for i in data:
+            d = dict(data[i])
+
+            print(d['combo'])
+
+            #print(data[i])
+            #for combo in data[i]:
+                #c = combo['combo']
+                #print(combo)
+
+       # https: // api.openweathermap.org / data / 2.5 / weather?lat = {lat} & lon = {lon} & appid = {APIkey}
         if len(c1Var) == 1:
             url = f"https://api.openweathermap.org/data/2.5/weather?q={c1Var[0]}&appid={API_key}"
             res = requests.get(url)
@@ -76,6 +98,7 @@ def get_weather(city):
             e1.insert(0, c1Var[0])
         else:
             c1.configure(values=c1Var)
+            #c1.configure(data["combo"])
             c1.set(c1Var[0])
             return None
 
@@ -84,7 +107,7 @@ def get_weather(city):
     weather = res.json()
     icon_id = weather['weather'][0]['icon']
     temperature = weather['main']['temp'] - 273.15
-    description = get_cesky(weather['weather'][0]['description'])
+    description = get_cesky_pocasi(weather['weather'][0]['description'])
     city = weather['name']
     country = weather['sys']['country']
 
